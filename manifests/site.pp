6e3cafd128c9c1246459692820589c585c809fca
Scott Robarts <scott@scottrobarts.com>
2020-12-26 15:02:37 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,6 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'my readme file',
-    owner => 'root',
-    } 
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
 }
