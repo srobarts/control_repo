675696fbaa6bf86965ba011ac769abc790a04c5c
Scott Robarts <scott@scottrobarts.com>
2020-12-27 18:37:38 -0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {'/opt/minecraft/minecraft_server.1.12.2.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
@@ -11,13 +11,13 @@ class minecraft {
   }
   file {'/opt/minecraft/eula.txt':
     ensure => file,
-    content => 'eula=true',
+    content => 'eula=true'
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service {'minecraft':
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
