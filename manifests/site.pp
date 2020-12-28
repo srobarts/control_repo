600c9f8b41c9060f51d89e3d20691f1095900333
Scott Robarts <scott@scottrobarts.com>
2020-12-27 18:46:27 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,11 +8,6 @@ node default {
 
 node 'master.puppet.vm' {
   include role::master_server
-  file {'/root/README':
-    ensure => file,
-    content => “Welcome to ${fqdn}”,
-    owner => 'root',
-  }
 }
 
 node 'minetest.puppet.vm': {
